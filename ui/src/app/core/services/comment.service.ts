d026891ec2dbb0a64a9ee86da5c484716d3d3205
adityaraaz <adityarajsingh00721@gmail.com>
2026-07-29 09:52:29 +0530

    Changed Stomp

--- ui/src/app/core/services/comment.service.ts
+++ ui/src/app/core/services/comment.service.ts
@@ -1,36 +1,51 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "../../environment";
-import SockJS from 'sockjs-client';
+// import SockJS from 'sockjs-client';
 // import { Client, over } from 'stompjs';
-import Stomp from 'stompjs';
+// import Stomp from 'stompjs';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
 import { CommentModel } from "../models/comment.model";
 
 @Injectable({
     providedIn:'root'
 })
 
-export class CommentService{
-    url : string = environment.apiUrl + '/comments';
-    constructor(private httpClient:HttpClient){}
+export class CommentService {
+  url: string = environment.apiUrl + '/comments';
 
-    private stompClient: Stomp.Client | null = null;
-    
+  constructor(private httpClient: HttpClient) {}
 
-    connect(onCommentReceived: (comment: CommentModel) => void) {
-        const socket = new SockJS('http://localhost:8080/ws'); // backend endpoint
-        this.stompClient = Stomp.over(socket);
+  private stompClient: Client | null = null;
 
-        this.stompClient.connect(
-            { Authorization: 'Bearer ' + localStorage.getItem('token') }, () => {
-        console.log('Connected to WebSocket');
-        this.stompClient?.subscribe('/topic/comments', (message) => {
-            onCommentReceived(JSON.parse(message.body));
-        });
-        });
-    }
+  connect(onCommentReceived: (comment: CommentModel) => void) {
+    this.stompClient = new Client({
+      // If your backend supports raw WebSocket:
+      brokerURL: 'ws://localhost:8080/ws',
 
-    public deleteComments(commentId : number){
-        return this.httpClient.delete(`${this.url}/${commentId}`)
-    }
+      // If you need SockJS fallback:
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+
+      connectHeaders: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+
+      reconnectDelay: 5000,
+    });
+
+    this.stompClient.onConnect = () => {
+      console.log('Connected to WebSocket');
+      this.stompClient?.subscribe('/topic/comments', (message) => {
+        onCommentReceived(JSON.parse(message.body));
+      });
+    };
+
+    this.stompClient.activate();
+  }
+
+  public deleteComments(commentId: number) {
+    return this.httpClient.delete(`${this.url}/${commentId}`);
+  }
 }
